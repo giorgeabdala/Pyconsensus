3dc57e5f68b7d689e3ac35199adebda6ae7d9c12
giorgeabdala <53539918+giorgeabdala@users.noreply.github.com>
2019-08-01 17:57:05 -0300

    Update Pyconsensus.py
    extração e tratamento dos dados em excel da blomberg.

--- Pyconsensus.py
+++ Pyconsensus.py
@@ -1,6 +1,24 @@
-import pandas as pd
+#requisitos: panda, xlrd
+
 
 import pandas as pd
 excel_file = 'consenso.xlsx'
-consenso = pd.read_excel(excel_file)
+
+#define nome das colunas do DF final
+COLUMN_NAME = ['ticker', 'target', 'consenso', 'qtdInst', 'qtdCompra', 'qtdNeutro', 'qtdVenda']
+
+#carrega o arquivo excel pulando as primeiras 9 linhas
+df = pd.read_excel(excel_file, skiprows=9)
+
+#deleta as colunas 0,2 e 5 (Unamed, Empresa, preço e Upside/Downside)
+df = df.drop(df.columns[[0,2,3,5]], axis=1)
+
+#renomeado colunas
+df = df.columns = COLUMN_NAME
+
+#faz um replace na coluna ticker e retira o ' BS'
+df['ticker'] = df['ticker'].str.replace(' BS', '')
+
+
+
 
