e44e5d3613ab7cb11662426f1ec038636ad89a8c
giorgeabdala <53539918+giorgeabdala@users.noreply.github.com>
2019-08-05 17:17:27 -0300

    Update Pyconsensus.py
    Add codigo pra retirar a data do excel da blomberg.

--- Pyconsensus.py
+++ Pyconsensus.py
@@ -1,4 +1,4 @@
-#requisitos: panda, xlrd
+#requisitos: panda, xlrd, 
 
 
 import pandas as pd
@@ -8,16 +8,26 @@ excel_file = 'consenso.xlsx'
 COLUMN_NAME = ['ticker', 'target', 'consenso', 'qtdInst', 'qtdCompra', 'qtdNeutro', 'qtdVenda']
 
 #carrega o arquivo excel pulando as primeiras 9 linhas
-df = pd.read_excel(excel_file, skiprows=9)
+df = pd.read_excel(excel_file)
+
+#pega a data do arquivo
+date = df.iloc[4,1]
+
+#deleta as 9 priemiras linhas
+df = df.drop(df.index[0:9])
 
 #deleta as colunas 0,2 e 5 (Unamed, Empresa, preço e Upside/Downside)
 df = df.drop(df.columns[[0,2,3,5]], axis=1)
 
 #renomeado colunas
-df = df.columns = COLUMN_NAME
+df.columns = COLUMN_NAME
 
 #faz um replace na coluna ticker e retira o ' BS'
 df['ticker'] = df['ticker'].str.replace(' BS', '')
+
+
+
+
 
 
 
